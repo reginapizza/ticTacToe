c29bcf3e8c8fbd8cd40ecc6be5a2f5d0a1964e5d
reginapizza <reginaelyse@gmail.com>
2019-12-11 10:04:40 -0500

    Added stylinging and cleaned up some code

--- assets/scripts/auth/events.js
+++ assets/scripts/auth/events.js
@@ -4,34 +4,15 @@ const getFormFields = require('../../../lib/get-form-fields.js')
 const api = require('./api')
 const ui = require('./ui')
 
-const onSignUpButton = function (event) {
-  $('.sign-up-start-button').hide()
-  $('.sign-up-start').show()
-}
-
 const onSignUp = function (event) {
   event.preventDefault()
 
   const form = event.target
   const formData = getFormFields(form)
   api.signUp(formData)
-    .then(ui.onSignUpSuccess)
+    .then(() => api.signIn(formData))
+    .then(ui.onSignInSuccess)
     .catch(ui.onSignUpFailure)
-}
-
-const onSignInButton = function (event) {
-  $('.sign-in-start-button').hide()
-  $('.sign-in-start').show()
-}
-
-const onChangePasswordButton = function (event) {
-  $('.change-password-start-button').hide()
-  $('.change-password, .change-password-start').css('display', 'block')
-}
-
-const onCancelButton = function (event) {
-  $('.change-password').hide()
-  $('.change-password-start-button').show()
 }
 
 const onSignIn = function (event) {
@@ -64,12 +45,8 @@ const onSignOut = function (event) {
     .catch(ui.onSignOutFailure)
 }
 module.exports = {
-  onSignUpButton,
   onSignUp,
-  onSignInButton,
   onSignIn,
-  onChangePasswordButton,
   onChangePassword,
-  onCancelButton,
   onSignOut
 }
--- assets/scripts/auth/ui.js
+++ assets/scripts/auth/ui.js
@@ -3,64 +3,91 @@
 const store = require('../store')
 // require so we can save the user and their token
 
-const onSignUpSuccess = function () {
-  $('#message').text('Signed Up Successfully!')
-  $('.sign-up').trigger('reset')
+const onSignUpButton = function (event) {
+  $('.sign-up-start-button').hide()
+  $('.sign-up').show()
+}
+
+const onSignInButton = function (event) {
+  $('.sign-in-start-button').hide()
+  $('.sign-in').show()
+}
+
+const onChangePasswordButton = function (event) {
+  $('.change-password, .change-password-start').css('display', 'block')
+}
+
+const onCancelButton = function (event) {
+  $('.change-password').hide()
+  $('.change-password-start-button').show()
+}
+
+const onSignUpCancel = function (event) {
+  $('.sign-up').hide()
+  $('.sign-up-start-button').show()
+}
+
+const onSignInCancel = function (event) {
+  $('.sign-in').hide()
+  $('.sign-in-start-button').show()
 }
 
 const onSignUpFailure = function () {
-  $('#message').text('Signed Up Failed.')
-  $('.sign-up').trigger('reset')
+  $('.auth-message').text('Sign Up Failed. Please Try Again.').fadeIn(0, 1)
+  $('.auth-message').text('Sign Up Failed. Please Try Again.').fadeOut(5000, 0)
+  $('.sign-up-form').trigger('reset')
 }
 
 const onSignInSuccess = function (responseData) {
-  $('#message').text('Signed In Successfully!')
+  $('.auth-message').text('Signed In Successfully!').fadeIn(0, 1)
+  $('.auth-message').text('Signed In Successfully!').fadeOut(5000, 0)
   store.user = responseData.user
   $('.sign-up, .sign-up-start-button, .sign-in').hide()
   $('#messageBox').show()
-  $('.change-password-title').show()
-  $('.change-password-start-button').show()
-  $('.sign-out').css('display', 'block')
-  $('#new-game').css('display', 'block')
-  $('.player-stats').css('display', 'inline-block')
-  $('.sign-in').trigger('reset')
+  $('.new-game').css('display', 'block')
+  $('.player-stats, .change-password, .sign-out').css('display', 'inline-block')
+  $('.sign-in-form , .sign-up-form').trigger('reset')
 }
 
 const onSignInFailure = function () {
-  $('#message').text('Sign in failed.')
-  $('.sign-in').trigger('reset')
+  $('.auth-message').text('Sign in failed. Please Try Again.').fadeIn(0, 1)
+  $('.auth-message').text('Sign in failed. Please Try Again.').fadeOut(5000, 0)
+  $('.sign-in-form').trigger('reset')
 }
 
 const onChangePasswordSuccess = function () {
-  $('#message').text('Password changed successfully!')
-  $('.change-password').trigger('reset')
+  $('.auth-message').text('Password changed successfully!').fadeIn(0, 1)
+  $('.auth-message').text('Password changed successfully!').fadeOut(5000, 0)
+  $('#changePassword').modal('hide')
+  $('.change-password-form').trigger('reset')
 }
 
 const onChangePasswordFailure = function () {
-  $('#message').text('Password change failed.')
-  $('.change-password').trigger('reset')
+  $('.auth-message').text('Password change failed. Please Try Again.').fadeIn(0, 1)
+  $('.auth-message').text('Password change failed. Please Try Again.').fadeOut(5000, 0)
+  $('.change-password-form').trigger('reset')
 }
 
 const onSignOutSuccess = function () {
-  $('#message').text('Signed out successfully!')
-  $('.sign-up, .sign-in, .sign-up-start-button, .sign-in-start-button').show()
-  $('.change-password, .sign-out, .sign-up-start, .sign-in-start, .change-password-start-button').hide()
-  $('.board').hide()
-  $('.player-stats').hide()
-  $('#new-game').hide()
-  $('#messageBox').hide()
-  $('.sign-out').trigger('reset')
+  $('.auth-message').text('Signed out successfully!').fadeIn(0, 1)
+  $('.auth-message').text('Signed out successfully!').fadeOut(5000, 0)
+  $('.sign-up-start-button, .sign-in-start-button').show()
+  $('.change-password, .sign-out, .change-password-start-button, .player-stats, .board, .new-game, #messageBox, .sign-up, .sign-in').hide()
   $('#total-games-number').text('')
-  $('.change-password-title').hide()
 }
 
 const onSignOutFailure = function () {
-  $('#message').text('Sign out failed')
-  $('.sign-out').trigger('reset')
+  $('.auth-message').text('Sign out failed. Please Try Again.').fadeIn(0, 1)
+  $('.auth-message').text('Sign out failed. Please Try Again.').fadeOut(5000, 0)
 }
 
 module.exports = {
-  onSignUpSuccess,
+  onSignUpButton,
+  onSignInButton,
+  onSignUpCancel,
+  onSignInCancel,
+  onChangePasswordButton,
+  onCancelButton,
   onSignUpFailure,
   onSignInSuccess,
   onSignInFailure,
